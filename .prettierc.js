4b29b1bab038be7b113280bf28711e017a72c3f2
Arijan Jašarević <arijan.jasarevic@superius.hr>
2024-01-17 12:05:25 +0100

    Added config for prettier and fixed tsconfig linking

---
+++ .prettierc.js
@@ -0,0 +1,9 @@
+export default {
+  printWidth: 125,
+  tabWidth: 2,
+  semi: true,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  arrowParens: "always",
+  plugins: ["prettier-plugin-tailwindcss"],
+};
